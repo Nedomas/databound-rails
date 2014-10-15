bcc267b6fd1053928b1c741a14385088d9397512
Domas <domas.bitvinskas@me.com>
2014-10-16 02:27:11 +0300

    Add #create specs

--- spec/controllers/godfather_spec.rb
+++ spec/controllers/godfather_spec.rb
@@ -1,9 +1,27 @@
 require 'spec_helper'
 
 describe UsersController, type: :controller do
-  describe 'raise error when model undefined' do
-    it 'requires a name' do
-      post(:create, { scope: '{}', data: '{}', extra_find_scopes: '[]' })
+  describe '#create' do
+    before :each do
+      data = {
+        scope: {},
+        data: {
+          name: 'John',
+        },
+        extra_find_scopes: [],
+      }
+
+      post(:create, javascriptize(data))
+    end
+
+    it 'responds consistently to js' do
+      expect(rubize(response)).to eq(success: true, id: 1)
+    end
+
+    it 'creates the record' do
+      user = User.find(1)
+      user_attributes = user.attributes.to_options
+      expect(user_attributes.slice(:id, :name)).to eq(id: 1, name: 'John')
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,13 @@ require 'pry'
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
 end
+
+def javascriptize(ruby_obj)
+  ruby_obj.each_with_object({}) do |(key, val), obj|
+    obj[key] = JSON.dump(val)
+  end
+end
+
+def rubize(response)
+  JSON.parse(response.body).to_options
+end
