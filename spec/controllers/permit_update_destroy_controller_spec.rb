9a4c346b3a87bcddcc5bea99d2edd142c0ef947c
Domas <domas.bitvinskas@me.com>
2015-01-03 20:15:22 +0200

    Add additional spec for destroy permissions

--- spec/controllers/permit_update_destroy_controller_spec.rb
+++ spec/controllers/permit_update_destroy_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe PermitUpdateDestroyController, type: :controller do
+  CURRENT_USER_ID = 1
+
+  before :each do
+    Project.create(city: 'LA', user_id: 5)
+    Project.create(city: 'LA', user_id: 1)
+  end
+
   describe '#update' do
-    CURRENT_USER_ID = 1
-
-    before :each do
-      Project.create(city: 'LA', user_id: 5)
-      Project.create(city: 'LA', user_id: 1)
-    end
-
     it 'raise when scope is not permitted' do
       data = {
         data: {
@@ -34,6 +34,33 @@ describe PermitUpdateDestroyController, type: :controller do
       }
 
       expect { post(:update, javascriptize(data)) }.not_to raise_error
+    end
+  end
+
+  describe '#destroy' do
+    it 'raise when scope is not permitted' do
+      data = {
+        data: {
+          id: 1,
+        },
+        scope: {},
+      }
+
+      expect { post(:destroy, javascriptize(data)) }.to raise_error(
+        Databound::NotPermittedError,
+        'Request for update or destroy not permitted',
+      )
+    end
+
+    it 'should destroy when param is permitted' do
+      data = {
+        data: {
+          id: 2,
+        },
+        scope: {},
+      }
+
+      expect { post(:destroy, javascriptize(data)) }.not_to raise_error
     end
   end
 end
